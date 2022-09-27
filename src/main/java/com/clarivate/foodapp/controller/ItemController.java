4af51197ce5facfc8755f902e07935dad2ef73ef
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-27 20:04:11 +0530

    Updated Item controller

--- src/main/java/com/clarivate/foodapp/controller/ItemController.java
+++ src/main/java/com/clarivate/foodapp/controller/ItemController.java
@@ -1,5 +1,48 @@
 package com.clarivate.foodapp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.clarivate.foodapp.dao.ResponseStructure;
+import com.clarivate.foodapp.dto.Item;
+import com.clarivate.foodapp.services.ItemService;
+
+@RestController
 public class ItemController {
 
+	@Autowired
+	ItemService itemService;
+
+	@PostMapping("/items")
+	public ResponseStructure<Item> saveItem(@RequestBody Item item) {
+		return itemService.saveItem(item);
+	}
+
+	@GetMapping("/getItems")
+	public ResponseStructure<List<Item>> getAllItems() {
+		return itemService.getAllItems();
+	}
+
+	@GetMapping("/getItems/{id}")
+	public ResponseStructure<Item> getItemById(@PathVariable int id) {
+		return itemService.getItemById(id);
+	}
+
+	@DeleteMapping("/items/{id}")
+	public ResponseStructure<String> deleteItem(@PathVariable int id) {
+		return itemService.deleteItem(id);
+	}
+
+	@PutMapping("/items")
+	public ResponseStructure<Item> updateItem(@RequestBody Item item) {
+		return itemService.updateItem(item);
+	}
 }
