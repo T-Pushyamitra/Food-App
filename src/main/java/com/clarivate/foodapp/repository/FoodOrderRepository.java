c9a43135af0245c8a52a78f68528460b36955a43
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-27 08:44:29 +0530

    Created Repositories

---
+++ src/main/java/com/clarivate/foodapp/repository/FoodOrderRepository.java
@@ -0,0 +1,9 @@
+package com.clarivate.foodapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.clarivate.foodapp.dto.FoodOrder;
+
+public interface FoodOrderRepository  extends JpaRepository<FoodOrder, Integer>{
+
+}
