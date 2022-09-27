7813ae8d1f75c73b2455d84e886df18cff8ce64f
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-27 08:42:17 +0530

    Created methods

--- src/main/java/com/clarivate/foodapp/dao/UserDao.java
+++ src/main/java/com/clarivate/foodapp/dao/UserDao.java
@@ -1,21 +1,25 @@
 package com.clarivate.foodapp.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import com.clarivate.foodapp.dto.User;
 import com.clarivate.foodapp.repository.userRepository;
 
-@Repository
+@Component
 public class UserDao {
 
-	
 	@Autowired
 	userRepository userRepository;
-	
+
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}
-	
+
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
 
 }
