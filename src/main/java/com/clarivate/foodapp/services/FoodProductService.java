ddfb6b7bc9c3dcd948ef34aa1119cd98071480ee
U6071310 <U6071310@CLVITN-CWTWTG3>
2022-09-27 14:46:53 +0530

    removed commented lines

--- src/main/java/com/clarivate/foodapp/services/FoodProductService.java
+++ src/main/java/com/clarivate/foodapp/services/FoodProductService.java
@@ -30,30 +30,3 @@ public class FoodProductService {
 
 
 
-
-/*
-package com.clarivate.foodapp.services;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.clarivate.foodapp.dao.ItemDao;
-import com.clarivate.foodapp.dto.Item;
-
-@Service
-public class ItemService {
-
-	@Autowired
-	ItemDao itemDao;
-	
-	public Item saveItem(Item item) {
-		return itemDao.addItem(item);
-	}
-	
-	public List<Item> getAllItems(){
-		return itemDao.getAllItems();
-	}
-	
-}*/
