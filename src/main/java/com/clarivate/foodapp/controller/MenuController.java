a780e0f0b0d968f64db8405e94290f133c1fb7a6
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-28 15:31:01 +0530

    Created New Controller and DAo for menu

--- src/main/java/com/clarivate/foodapp/controller/MenuController.java
+++ src/main/java/com/clarivate/foodapp/controller/MenuController.java
@@ -3,13 +3,16 @@ package com.clarivate.foodapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.clarivate.foodapp.dao.ResponseStructure;
+import com.clarivate.foodapp.dto.Item;
 import com.clarivate.foodapp.dto.Menu;
 import com.clarivate.foodapp.dto.User;
 import com.clarivate.foodapp.services.MenuService;
@@ -32,5 +35,15 @@ public class MenuController {
 	@GetMapping("/menu/{id}")
 	public ResponseStructure<Menu> getMenuById(@PathVariable int id) {
 		return menuService.getMenuById(id);
+	}
+	
+	@DeleteMapping("/menu/{id}")
+	public ResponseStructure<String> deleteMenu(@PathVariable int id) {
+		return menuService.deleteMenu(id);
+	}
+
+	@PutMapping("/menu")
+	public ResponseStructure<Menu> updateMenu(@RequestBody Menu menu) {
+		return menuService.updateMenu(menu);
 	}
 }
--- src/main/java/com/clarivate/foodapp/dao/MenuDao.java
+++ src/main/java/com/clarivate/foodapp/dao/MenuDao.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.clarivate.foodapp.dto.Menu;
-import com.clarivate.foodapp.dto.User;
 import com.clarivate.foodapp.repository.MenuRepository;
 
 @Component
