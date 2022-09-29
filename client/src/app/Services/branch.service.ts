7f74c46023728a6f269bece60aa6bc7aa05149b6
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-29 15:42:08 +0530

    Implemented working of branch component

--- client/src/app/Services/branch.service.ts
+++ client/src/app/Services/branch.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,25 @@ import { Injectable } from '@angular/core';
 })
 export class BranchService {
 
-  constructor() { }
+  
+  getUrl: string = "http://localhost:8080/branch/1"
+  
+  postUrl: string = "http://localhost:8080/foodProduct/1"
+
+  deleteUrl: string = "http://localhost:8080/foodProduct/"
+
+  constructor(private http:HttpClient) { }
+
+  getMenu(){
+    return this.http.get(this.getUrl);
+  }
+
+  addFoodProduct(foodProduct : any){
+    return this.http.post(this.postUrl,foodProduct,{responseType : "json"});
+  }
+
+  deleteFoodProduct(id:any){
+    return this.http.delete(this.deleteUrl+`${id}`);
+  }
+
 }
--- client/src/app/branch/branch.component.ts
+++ client/src/app/branch/branch.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BranchService } from '../Services/branch.service';
+import { FoodProduct } from './foodProduct';
 
 @Component({
   selector: 'app-branch',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./branch.component.css']
 })
 export class BranchComponent implements OnInit {
+  results: any;
+  error : any;
+  errorMessage : any;
 
-  constructor() { }
+  
+
+  constructor(private branch:BranchService, private router:Router) { }
 
   ngOnInit(): void {
+    this.branch.getMenu().subscribe((data) =>{
+        this.results = data;
+
+        console.log(this.results);
+    },(err) => {
+      this.error = err;
+      this.errorMessage = err.message;
+      console.log(this.errorMessage);
+      console.log(this.error);
+    });
+  }
+
+  productId(id:any){
+    return id;
+  }
+
+  addFoodProduct(newFoodProduct : NgForm): any{
+    newFoodProduct.value.availability = true
+    this.branch.addFoodProduct(newFoodProduct.value).subscribe((res) => {
+      console.log(res);
+      this.router.navigate(['/menu']);
+      this.branch.getMenu().subscribe((data) => {
+        this.results = data;
+      });
+    })
+  }
+
+  updateFoodProduct(updateFoodProduct : NgForm): any{
+
+  }
+  deleteFoodProduct(id:any){
+    console.log(id)
+    this.branch.deleteFoodProduct(id).subscribe((res) =>{
+      console.log(res);
+      this.router.navigate(['/menu']);
+      this.branch.getMenu().subscribe(
+        (data) => {
+          this.results = data;
+        },
+      );
+    })
   }
 
 }
