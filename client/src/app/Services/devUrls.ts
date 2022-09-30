871e65c251102ee0c0932b7d096314e5563d7eb4
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-30 17:33:41 +0530

    Updated with new urls

--- client/src/app/Services/devUrls.ts
+++ client/src/app/Services/devUrls.ts
@@ -1,6 +1,7 @@
 export class Urls {
   public static foodOrderUrl: string = 'http://localhost:8080/foodOrder/3';
-  public static itemsUrl: string = 'http://localhost:8080/items/3';
+  public static itemsUrl: string = 'http://localhost:8080/items/';
   public static foodProductUrl: string = 'http://localhost:8080/foodProduct/';
-  public static userUrl: string = "http://localhost:8080/users"
+  public static userUrl: string = "http://localhost:8080/users";
+  public static menuUrl: string = "http://localhost:8080/menu/1";
 }
