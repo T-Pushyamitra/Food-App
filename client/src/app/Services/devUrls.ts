c79cff0a732aed45cb52f138f2e5a4e96acbbef5
Thammineni Pushyamitra <thamminenipushyamitra@clarivate.com>
2022-09-30 10:38:58 +0530

    Init commit

---
+++ client/src/app/Services/devUrls.ts
@@ -0,0 +1,6 @@
+export class Urls {
+  public static foodOrderUrl: string = 'http://localhost:8080/foodOrder/3';
+  public static itemsUrl: string = 'http://localhost:8080/items/3';
+  public static foodProductUrl: string = 'http://localhost:8080/foodProduct/';
+  public static userUrl: string = "http://localhost:8080/users"
+}
