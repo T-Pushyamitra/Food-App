0923853d60a3ca1a8772fab574e01d4bc4ead58d
Suresh V <vaddigangannaboya.suresh@clarivate.com>
2022-10-01 12:25:39 +0530

    init commit

---
+++ client/src/app/invoicegenerator/invoicegenerator.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-invoicegenerator',
+  templateUrl: './invoicegenerator.component.html',
+  styleUrls: ['./invoicegenerator.component.css']
+})
+export class InvoicegeneratorComponent implements OnInit {
+ 
+ result:any
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
